1912b1fe17f05a53361fd2ba157063bafd0ae36d
AnastasiaGer <aagermanova@gmail.com>
2021-10-11 12:53:29 -0700

    start

---
+++ script.js
@@ -0,0 +1,20 @@
+function slidesPlugin(activeSlide = 0) {
+  const sliders = document.querySelectorAll('.slide')
+
+  sliders[activeSlide].classList.add('active');
+
+  for (const slide of sliders) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active');
+    })
+  }
+  
+  function clearActiveClasses() {
+    sliders.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
+}
+
+slidesPlugin()
